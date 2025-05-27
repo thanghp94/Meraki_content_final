a1bc93c8c9a97a3c78f2d46b7a5a94fd6455cb2e
Thắng Huỳnh Phan <thanghuynh@meraki.edu.vn>
2025-05-27 10:10:45 +0000

    npm error Missing script: "dev" npm error npm error To see a list of scripts, run: npm error   npm run npm error A complete log of this run can be found in: /Users/victor/.npm/_logs/2025-05-27T10_09_03_321Z-debug-0.log victor@iMac-cua-thang ~ %

---
+++ src/lib/firebase.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, // Optional
+};
+
+// Initialize Firebase
+// To prevent re-initialization on hot reloads in Next.js development,
+// check if an app has already been initialized.
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { app, db, auth, storage, firebaseConfig };
